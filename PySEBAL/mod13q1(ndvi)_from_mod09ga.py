a1df66b09b19d25964805fa6a4dc936b21a994f5
Reza Hosseini <56230851+BlueGIS760404@users.noreply.github.com>
2025-08-03 15:39:42 +0330

    Update mod13q1(ndvi)_from_mod09ga.py

--- PySEBAL/mod13q1(ndvi)_from_mod09ga.py
+++ PySEBAL/mod13q1(ndvi)_from_mod09ga.py
@@ -1,3 +1,4 @@
+# OSGeo4W
 gdal_translate "HDF4_EOS:EOS_GRID:MOD09GA.hdf:MODIS_Grid_500m_2D:sur_refl_b01_1" band1.tif
 gdal_translate "HDF4_EOS:EOS_GRID:MOD09GA.hdf:MODIS_Grid_500m_2D:sur_refl_b02_1" band2.tif
 
